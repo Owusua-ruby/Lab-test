2ebd9db75b24b8152ac166b70b7676b56b9906a2
Owusua_brooks <75069388+Owusua-ruby@users.noreply.github.com>
2021-04-11 18:50:44 +0100

    Add files via upload

---
+++ Firstlab.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string username, password;
+    cout<<"Enter your username";
+    cin>>"username:";
+    cout<<"Enter your password";
+    cin>>"password:";
+
+    if(username=="Frank" && password=="Frank1234"){
+        cout<<"Welcome to Chigolo"<<endl;
+        cout<<"Your balance is GHC 500"<<endl;
+            }
+            else
+                cout<<"oops, you do not have an account here"<<endl;
+                
+}
